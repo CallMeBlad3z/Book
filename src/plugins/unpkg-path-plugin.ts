251fbb826e7507a0888d6b52673787b0497f35fc
Blad3z <santtu366@live.com>
2025-03-17 22:15:26 +0200

    Remove unused axios and localForage imports from unpkg path plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,15 +1,4 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
-import localForage from 'localforage';
-
-const fileCache = localForage.createInstance({
-  name: 'filecache',
-});
-
-(async () => {
-  const cache = await fileCache.getItem('key');
-  console.log(cache);
-})();
 
 
 export const unpkgPathPlugin = () => {
@@ -35,38 +24,6 @@ export const unpkgPathPlugin = () => {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
         };
-    });
- 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
- 
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
-        
-        // Check to see if we have already fetched this file
-        // and if it is in the cache
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-
-        // If it is, return it immediately
-        if (cachedResult) {
-          return cachedResult;
-        }
-        
-        const { data, request } = await axios.get(args.path);
-
-        const result: esbuild.OnLoadResult = {
-          loader: 'jsx',
-          contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname,
-        };
-        // store response in cache
-        await fileCache.setItem(args.path, result);
-
-        return result;
       });
     },
   };
