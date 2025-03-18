64458fc3bcce227e8f9f9a9dea4172f1fb7054b7
Blad3z <santtu366@live.com>
2025-03-18 23:37:31 +0200

    Wrap CodeCell content in Resizable component for improved layout flexibility

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import CodeEditor from ".//code-editor";
 import Preview from ".//preview";
 import bundle from "../bundler";
+import Resizable from "./resizable";
 
 const CodeCell = () => {
   const [code, setCode] = useState("");
@@ -13,15 +14,17 @@ const CodeCell = () => {
   };
   
   return (
+    <Resizable direction="vertical">
     <div>
       <CodeEditor initialValue="const a = 1;"
-      onChange={(value) => setInput(value)}
+        onChange={(value) => setInput(value)}
       />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
       <Preview code={code} />
     </div>
+    </Resizable>
   );
 };
 
