01e6b5e3a6e2e99064aad3d42c1cdbcbdaf82205
Blad3z <santtu366@live.com>
2025-03-19 23:10:07 +0200

    Update Resizable component to set maxConstraints for improved height adaptability

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -7,7 +7,14 @@ interface ResizableProps {
   }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-    return <ResizableBox width={Infinity} height={300} resizeHandles={["s"]}>{children}</ResizableBox>;
+    return <ResizableBox 
+    maxConstraints={[Infinity, window.innerHeight * 0.9]}
+    width={Infinity}
+    height={300}
+    resizeHandles={["s"]}
+    >
+      {children}
+    </ResizableBox>;
 }
 
 export default Resizable;
