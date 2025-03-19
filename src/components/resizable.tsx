cd49622f0dd35c15def2826fe06b34a155d08d7c
Blad3z <santtu366@live.com>
2025-03-19 23:19:11 +0200

    Enhance Resizable component with dynamic constraints based on direction for improved resizing behavior

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,4 @@
-import { ResizableBox } from 'react-resizable';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 import './resizable.css';
 
 interface ResizableProps {
@@ -7,6 +7,29 @@ interface ResizableProps {
   }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  let resizableProps: ResizableBoxProps;
+
+  if (direction === "horizontal") {
+    resizableProps = {
+      className: "resize-horizontal",
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      height: Infinity,
+      width: window.innerWidth * 0.75,
+      resizeHandles: ["e"],
+    };
+  }
+  else {
+    resizableProps = {
+      className: "resize-vertical",
+      minConstraints: [Infinity, window.innerHeight * 0.1],
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      height: 300,
+      width: Infinity,
+      resizeHandles: ["s"],
+    };
+  }
+
     return <ResizableBox 
     maxConstraints={[Infinity, window.innerHeight * 0.9]}
     minConstraints={[Infinity, window.innerHeight * 0.25]}
