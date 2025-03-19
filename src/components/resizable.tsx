a7f7300500191d692ec20449945aa4f76fc44c44
Blad3z <santtu366@live.com>
2025-03-19 23:37:41 +0200

    Refactor Resizable component for improved code readability and maintainability

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,44 +1,34 @@
-import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 import './resizable.css';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
-    direction: "horizontal" | "vertical";
-    children?: React.ReactNode;
-  }
+  direction: 'horizontal' | 'vertical';
+  children?: React.ReactNode;  
+}
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
 
-  if (direction === "horizontal") {
+  if (direction === 'horizontal') {
     resizableProps = {
-      className: "resize-horizontal",
+      className: 'resize-horizontal',
       minConstraints: [window.innerWidth * 0.2, Infinity],
       maxConstraints: [window.innerWidth * 0.75, Infinity],
       height: Infinity,
       width: window.innerWidth * 0.75,
-      resizeHandles: ["e"],
+      resizeHandles: ['e'],
     };
-  }
-  else {
+  } else {
     resizableProps = {
-      className: "resize-vertical",
-      minConstraints: [Infinity, window.innerHeight * 0.1],
+      minConstraints: [Infinity, 24],
       maxConstraints: [Infinity, window.innerHeight * 0.9],
       height: 300,
       width: Infinity,
-      resizeHandles: ["s"],
+      resizeHandles: ['s'],
     };
   }
 
-    return <ResizableBox 
-    maxConstraints={[Infinity, window.innerHeight * 0.9]}
-    minConstraints={[Infinity, window.innerHeight * 0.25]}
-    width={Infinity}
-    height={300}
-    resizeHandles={["s"]}
-    >
-      {children}
-    </ResizableBox>;
-}
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
+};
 
 export default Resizable;
