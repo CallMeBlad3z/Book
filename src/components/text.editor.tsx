81bfbc73f9586d2d81984712da0e45d03beed6d8
Blad3z <santtu366@live.com>
2025-03-30 23:49:50 +0300

    Implement editing state in TextEditor component with click-to-edit functionality

--- src/components/text.editor.tsx
+++ src/components/text.editor.tsx
@@ -1,11 +1,34 @@
 import MDEditor from "@uiw/react-md-editor";
+import { useState, useEffect, useRef } from "react";
 
 const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = () => {
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div>
+        <MDEditor />
+      </div>
+    );
+  }
+
   return (
-    <div className="text-editor">
-      <MDEditor.Markdown source={'# Header'} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={'# Header'} />
+      </div>
     </div>
   );
-};
+}
 
 export default TextEditor;
