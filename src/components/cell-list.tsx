b114f6346a73be30570aac18fe97307f6143ba8c
Blad3z <santtu366@live.com>
2025-04-01 21:26:32 +0300

    Add CellList component to display cell list using useTypedSelector hook

---
+++ src/components/cell-list.tsx
@@ -0,0 +1,10 @@
+import { useTypedSelector } from "../hooks/use-typed-selector";
+
+const CellList: React.FC = () => {
+    useTypedSelector((state) => state);
+
+    return <div>CellList</div>;
+};
+
+export default CellList;
+
