d7ab7eecdd704519655b6e019156a28fddeba75b
Blad3z <santtu366@live.com>
2025-04-02 01:07:32 +0300

    Refactor Resizable component to improve resize event handling and ensure proper cleanup, while enhancing responsiveness by adjusting useEffect dependencies.

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -4,12 +4,11 @@ import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
-  children?: React.ReactNode;  
+  children?: React.ReactNode;
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
-
   const [innerHeight, setInnerHeight] = useState(window.innerHeight);
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
   const [width, setWidth] = useState(window.innerWidth * 0.75);
@@ -20,22 +19,20 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
       if (timer) {
         clearTimeout(timer);
       }
-      setTimeout(() => {
+      timer = setTimeout(() => {
         setInnerHeight(window.innerHeight);
         setInnerWidth(window.innerWidth);
         if (window.innerWidth * 0.75 < width) {
           setWidth(window.innerWidth * 0.75);
         }
-      }
-      , 100);
+      }, 100);
     };
-
     window.addEventListener('resize', listener);
 
     return () => {
       window.removeEventListener('resize', listener);
-    }
-  }, []);
+    };
+  }, [width]);
 
   if (direction === 'horizontal') {
     resizableProps = {
@@ -47,7 +44,7 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
       resizeHandles: ['e'],
       onResizeStop: (event, data) => {
         setWidth(data.size.width);
-      }
+      },
     };
   } else {
     resizableProps = {
