b493e57a7ae6beeda7957dc0f4d0b910c503defd
Blad3z <santtu366@live.com>
2025-04-02 01:05:35 +0300

    Refactor CodeCell component to update import paths and adjust useEffect timer duration, while improving layout styles for better responsiveness.

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import CodeEditor from "../components/code-editor";
-import Preview from "../components/preview";
+import CodeEditor from "./code-editor";
+import Preview from "./preview";
 import bundle from "../bundler";
 import Resizable from "./resizable";
 import { Cell } from "../state";
@@ -20,23 +20,30 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
       const output = await bundle(cell.content);
       setCode(output.code);
       setErr(output.err);
-    }, 1000);
+    }, 750);
 
     return () => {
       clearTimeout(timer);
     };
   }, [cell.content]);
-  
+
   return (
     <Resizable direction="vertical">
-    <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
-      <Resizable direction="horizontal">
-        <CodeEditor initialValue={cell.content}
-          onChange={(value) => updateCell(cell.id, value)}
-        />
-      </Resizable>
-      <Preview code={code} err={err} />
-    </div>
+      <div
+        style={{
+          height: 'calc(100% - 10px)',
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
+          />
+        </Resizable>
+        <Preview code={code} err={err} />
+      </div>
     </Resizable>
   );
 };
