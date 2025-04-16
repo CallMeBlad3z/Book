d4b70af445dcac44e38b482af9cb949c9f317da1
Blad3z <santtu366@live.com>
2025-04-16 23:37:16 +0300

    Update bundlesReducer to handle loading state for BUNDLE_START and BUNDLE_COMPLETE actions, ensuring proper state management during bundling process.

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -16,11 +16,21 @@ const reducer = produce(
   (state: BundlesState = initialState, action: Action): BundlesState => {
     switch (action.type) {
       case ActionType.BUNDLE_START:
-          return state;
+        state[action.payload.cellId] = {
+          loading: true,
+          code: "",
+          err: "",
+        };
+        return state;
       case ActionType.BUNDLE_COMPLETE:
-          return state;
+        state[action.payload.cellId] = {
+          loading: false,
+          code: action.payload.bundle.code,
+          err: action.payload.bundle.err,
+        };
+        return state;
       default:
-          return state;
+        return state;
     }
   }, initialState
 );
